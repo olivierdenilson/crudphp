2fc95b571884995d366cdb0dca3d4f3a5ddbae23
Denilson Alves Oliveira <denilson.oliveira@basis.com.br>
2020-02-25 17:31:33 -0300

    Criando o arquivo css

--- includes/head.php
+++ includes/head.php
@@ -6,6 +6,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Projeto CRUD </title>
 <link href="css/bootstrap-4.4.1.css" rel="stylesheet" type="text/css">
+<link href="css/folha.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php include_once("includes/head.php");?>
 
-
+<div class="container fundo">
     <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
       <h1 class="display-4"> PROJETO CRUD </h1>
       <p class="lead">Construção do Projeto para a pratica de estudo com PHP .</p>
     </div>
+</div>
 
     <?php include_once("includes/foot.php");?>
