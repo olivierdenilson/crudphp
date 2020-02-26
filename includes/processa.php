d9d7dd4b2935c054425a20f5332108ec551ccc16
Denilson Alves Oliveira <denilson.oliveira@basis.com.br>
2020-02-26 16:14:16 -0300

    corrigindo o link location

--- includes/processa.php
+++ includes/processa.php
@@ -12,10 +12,6 @@ $senhas = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_STRING);
 $senha= md5($senhas);
 
 
-//echo "Nome.: ". $nome ."<br>";
-//echo "E-mail.: " . $email . "<br>";
-//echo "Senha.:" . $senha . "<br>";
-
 // criando o script para incluir no banco de
 $result_usuario = "INSERT INTO usuarios (nome, email, senha, created) VALUES ('$nome','$email', '$senha', NOW())";
 
@@ -24,24 +20,14 @@ $resultado = mysqli_query($conn, $result_usuario);
 
 // validar os campos vazios
 
-if($nome==null){
-    $_SESSION['vazio'] = "<p style='color:red;'>Preencha o campo $nome </p>";
-}if($email==null){
-    $_SESSION['vazio'] = "<p style='color:red;'>Preencha o campo $email </p>";
-}if($senha==null){
-    $_SESSION['vazio'] = "<p style='color:red;'>Preencha o campo $senha </p>";
-}if(mysqli_insert_id($conn)){
+if(mysqli_insert_id($conn)){
     $_SESSION['msg'] = "<p style='color:green;'>Usuário cadastrado com sucesso</p>";
-	header("Location: /projetocrudphp/cadastro.php");
+	header("Location: /crud/crudphp/cadastro.php");
 }else{
     $_SESSION['msg'] = "<p style='color:red;'>Usuário não foi cadastrado com sucesso</p>";
-    header("Location: /projetocrudphp/cadastro.php");
+    header("Location: /crud/crudphp/cadastro.php");
 	
 }
-
-
-
-
 
 
 ?>
