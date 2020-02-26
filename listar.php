33ebbbe6a606150affed8b34d10240dc41730233
Denilson Alves Oliveira <denilson.oliveira@basis.com.br>
2020-02-26 13:36:35 -0300

    desenvolvimento da Paginação
    layout da paginação da listagem de dados

--- listar.php
+++ listar.php
@@ -6,6 +6,7 @@
 
 <h1> Listas -  Usuários </h1>
 
+<!--Estrutura da Tabela html -->
 <table class="table table-hover">
   <thead>
     <tr>
@@ -20,12 +21,23 @@
 
 <?php
     
-     // trazendo os resultado do banco de dados
-     $result_usuarios = "SELECT * FROM usuarios";
-     $resutado = mysqli_query($conn, $result_usuarios);
+    // Receber o número da página
+    $pagina_atual = filter_input(INPUT_GET,'pagina', FILTER_SANITIZE_NUMBER_INT);
+    $pagina = (!empty($pagina_atual))?$pagina_atual:1;
 
+    // Setar a quantidade de pagina de itens por página
+    $qnt_result_pg = 5;
+    $inicio = ($qnt_result_pg * $pagina) - $qnt_result_pg;
+
+
+     // trazendo os resultado do banco de dados
+     $result_usuarios = "SELECT * FROM usuarios LIMIT $inicio, $qnt_result_pg";
+     //execultando a instrução SQL
+     $resutado = mysqli_query($conn, $result_usuarios);
+    //percorrendo a linha no banco de dados 
      while($resultado = mysqli_fetch_assoc( $resutado))
 {?>
+
 
 <div>
   <tbody>
@@ -34,7 +46,8 @@
             <td><?php echo $resultado['nome'] ?></td>
             <td><?php echo $resultado['email'] ?></td>
             <td><?php echo $resultado['senha'] ?></td>
-            <td><a class="btn btn-outline-info" href="#">Editar</a>  <a  class="btn btn-outline-danger" href="#">Excluir</a></td>
+            <td><a class="btn btn-outline-info" href="#">Editar</a>
+            <a class="btn btn-outline-danger" href="#">Excluir</a></td>
     </tr>
 
 <?php } ?>
@@ -43,22 +56,78 @@
 </table>
 
 
+<?php
+
+    $result_pg = "SELECT COUNT(id) AS num_result FROM usuarios";
+    $resultado_pg = mysqli_query($conn , $result_pg);
+    $row_pg = mysqli_fetch_assoc($resultado_pg);
+
+    //echo $row_pg['num_result'];
+    $quantidade_pg = ceil($row_pg['num_result'] / $qnt_result_pg);
+
+    // Limitar os Links antes e depois
+    $max_links = 2;
+
+    /*echo "<a href='listar.php?pagina=1'> Primeira </a>";
+
+    for($pag_ant = $pagina - $max_links; $pag_ant <=$pagina -1; $pag_ant++) {
+        if($pag_ant >=1){
+          echo "<a href='listar.php?pagina=$pag_ant'>$pag_ant</a>";
+        }
+    }
+
+    echo "$pagina"; // página atual
+
+    for($pag_dep = $pagina +1 ; $pag_dep <=$pagina + $max_links; $pag_dep++){
+
+      if($pag_dep <= $quantidade_pg){
+         echo "<a href='listar.php?pagina=$pag_dep'>$pag_dep</a>";
+      }
+
+    }
+
+    echo "<a href='listar.php?pagina=$quantidade_pg'> Ultima </a>"; */
+
+
+?>
+
+<!-- Layout da Páginação -->
+
 <nav aria-label="Navegação de página exemplo">
   <ul class="pagination">
-    <li class="page-item"><a class="page-link" href="#">Anterior</a></li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item"><a class="page-link" href="#">2</a></li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item"><a class="page-link" href="#">Próximo</a></li>
+    <li class="page-item"><a class="page-link" href="listar.php?pagina=1">Anterior</a></li>
+    
+    <?php
+          for($pag_ant = $pagina - $max_links; $pag_ant <=$pagina -1; $pag_ant++) {
+          
+            if($pag_ant >=1){
+
+            echo "<li class='page-item'><a class='page-link' href='listar.php?pagina=$pag_ant'>$pag_ant</a></li>";
+          }
+        } ?>
+       
+       
+       <li class="page-item active">
+           <a class="page-link" href="listar.php?pagina=<?php echo $pagina?>"> <span class="sr-only">
+           </span><?php echo $pagina?></a>
+      </li>
+
+
+   <?php 
+        for($pag_dep = $pagina +1 ; $pag_dep <=$pagina + $max_links; $pag_dep++){
+
+          if($pag_dep <= $quantidade_pg){
+
+            echo "<li class='page-item'><a class='page-link' href='listar.php?pagina=$pag_dep'>$pag_dep</a></li>";
+
+          }
+    
+        }
+      
+    ?>
+       <li class="page-item"><a class="page-link" href="listar.php?pagina=<?php echo $quantidade_pg;?>">Ultima</a></li>
   </ul>
 </nav>
-
-
 </div>
 <?php include_once("includes/foot.php");?>
-
-
-
-
-
 
